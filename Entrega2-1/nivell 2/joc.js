5c633116b2507bec3f28982ec39faa06d88d84d4
Rocio Marchetti <alexcadevall@gmail.com>
2022-09-27 11:39:18 +0200

    classjugadors feta

---
+++ Entrega2-1/nivell 2/joc.js
@@ -0,0 +1,11 @@
+import { Jugadors } from "./jugadors.js";
+
+export class Marcador {
+
+    MarcadorActual () {
+        jugador1 = {username: "", puntuació : ""}
+        jugador2 = {username: "", puntuació : ""}
+    }
+
+}
+
