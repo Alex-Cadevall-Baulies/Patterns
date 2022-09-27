feb57a5f760b54f6d472393a8c4a2613bddbb97f
Rocio Marchetti <alexcadevall@gmail.com>
2022-09-27 11:54:05 +0200

    marcadorActual done

--- Entrega2-1/nivell 2/joc.js
+++ Entrega2-1/nivell 2/joc.js
@@ -3,8 +3,14 @@ import { Jugadors } from "./jugadors.js";
 export class Marcador {
 
     MarcadorActual () {
-        jugador1 = {username: "", puntuació : ""}
-        jugador2 = {username: "", puntuació : ""}
+        jugador1 = this.#player.username
+        puntuacio1 = 0
+        jugador2 = this.#player.username
+        puntuacio2 = 0
+        marcadorActual = (jugador1,  puntuacio1, jugador2, puntuacio2) => {
+        console.log(`user : ${jugador1} | score : ${puntuacio1}`);
+        console.log(`user: ${jugador2} | score : ${puntuacio2}`)
+        }
     }
 
 }
