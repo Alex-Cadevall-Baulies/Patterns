1ef9b91636970766eb6067810b984933f2d0a7ef
Rocio Marchetti <alexcadevall@gmail.com>
2022-10-05 13:05:16 +0200

    findwinner pending

--- Entrega3-1/nivell 2/app.js
+++ Entrega3-1/nivell 2/app.js
@@ -8,14 +8,16 @@ const player = new Jugador(data.player1.username, data.player1.email)
 const player2 = new Jugador(data.player2.username, data.player2.email)
 const player3 = new Jugador(data.player3.username, data.player3.email)
 
+const marcador = new Marcador
+marcador.addScore("badminton", player, 10)
+marcador.addScore("badminton", player, 20)
+marcador.addScore("badminton", player2, 15)
+marcador.addScore("escacs", player2, 1)
+marcador.addScore("badminton", player3, 5)
+marcador.addScore("escacs", player3, 0)
 
+console.log(marcador.scoreboard)
 
-const marcador1 = new Marcador("badminton", player, 10)
-const marcador2 = new Marcador("badminton", player, 20)
-const marcador3 = new Marcador("badminton", player2, 15)
-const marcador4 = new Marcador("escacs", player2, 1)
-const marcador5 = new Marcador("badminton", player3, 5)
-const marcador6 = new Marcador("escacs", player3, 0)
 
 
 /*
