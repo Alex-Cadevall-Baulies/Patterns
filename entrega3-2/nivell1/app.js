211ccffd85e25fcd4d3167841c77fb901a93006e
Nom Cognom <nomcognom@example.com>
2023-01-18 09:22:28 +0100

    solved changes

--- entrega3-2/nivell1/app.js
+++ entrega3-2/nivell1/app.js
@@ -1,11 +1,7 @@
 const Middleware = require("./mwm.js");
 const Maths = require("./calculator.js")
-const data = require('./data.json');
 
-let a = data.number1
-let b = data.number2
-
-const calculator = new Maths({a, b})
+const calculator = new Maths()
 const mwm = new Middleware(calculator)
 
 // Quadrat
@@ -19,8 +15,8 @@ mwm.use((req, next) => {
 
 // Cub
 mwm.use((req, next) => {
-    req.a = req.a * a;
-    req.b = req.b * b;
+    req.a = req.a * req.a * req.a;
+    req.b = req.b * req.b * req.b;
     console.log('cub a: ' + req.a)
     console.log('cub b: ' + req.b)
     next();
@@ -28,13 +24,18 @@ mwm.use((req, next) => {
 
 // Divisió entre els operands inicials
 mwm.use((req, next) => {
-    req.a = req.a / a;
-    req.b = req.b / b;
+    req.a = req.a / 2;
+    req.b = req.b / 2;
     console.log('divisió a: ' + req.a)
     console.log('divisió b: ' + req.b)
     next();
   });
 
-console.log('resultat suma: ' + mwm.sum({a, b}));
-console.log('resultat resta: ' + mwm.sub({a, b}));
-console.log('resultat multiplicació: ' + mwm.multiply({a, b}));
+console.log('resultat suma: ' + mwm.sum({a: 2, b: 4}));
+console.log('resultat resta: ' + mwm.sub({a: 3, b: 6}));
+console.log('resultat multiplicació: ' + mwm.multiply({a: 5, b: 10}));
+
+/*
+ L'exercici està bé, però als middlewares estàs barrejant req.a amb a (una variable externa que no hauria d'exisitr). 
+ I el tercer middleware ha de fer la divisio entre 2, no entre si mateix!
+*/
